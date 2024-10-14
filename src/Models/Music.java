72b807041a13a761c44d435993cb21ce372acc80
jpwiig <jpwiig@gmail.com>
2024-10-14 13:50:26 +0200

    cleaning up some comments

--- src/Models/Music.java
+++ src/Models/Music.java
@@ -2,10 +2,10 @@ package Models;
 
 public class Music {
     /**
-    @Param<String>music
-    @Param<String>artist
-    @Param<String>album
-    @Param<String[]>whereToListen
+    @Param String music
+    @Param String artist
+    @Param String album
+    @Param String[] whereToListen
  **/
 //getters and setters, constructur and tostring
 }
